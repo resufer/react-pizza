1f4fb9eea9f070f6ed4fb1bafc5bb242b1911e1d
resufer <66846932+resufer@users.noreply.github.com>
2021-08-17 14:07:52 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './scss/index.scss';
+import App from './App';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
