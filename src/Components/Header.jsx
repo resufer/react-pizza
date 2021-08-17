36ba5759b4fa5e6d0c1086bcd9d0ff2c1d63e33c
resufer <66846932+resufer@users.noreply.github.com>
2021-08-18 02:56:55 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="container">
-        <Link to='/'>
+        <Link to="/">
           <div className="header__logo">
             <img width="38" src={logo} alt="Pizza logo" />
             <div>
