f664200a5f58a60414aaa747ae13e0c4aa9fd8f1
resufer <66846932+resufer@users.noreply.github.com>
2021-08-19 03:34:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 import { Header } from './Components';
 import { Cart, Home } from './pages';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { setPizzas } from './redux/actions/pizzas';
-import { connect } from 'react-redux';
 
-class App extends React.Component {
-  componentDidMount() {
+export default function App() {
+  let dispatch = useDispatch();
+
+  React.useEffect(() => {
     axios.get('http://localhost:3000/db.json')
-    // .then(resp => window.store.dispatch(setPizzas(resp.data.pizzas)))
-      .then(resp => this.props.setPizzas(resp.data.pizzas))
-  }
+      .then(resp => dispatch(setPizzas(resp.data.pizzas)))
+  }, []);
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route path='/' exact render={() => <Home pizzas={this.props.items}/>} />
-          <Route path='/cart' render={() => <Cart />} />
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path='/' exact component={Home} />
+        <Route path='/cart' component={Cart} />
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-function MSTP(state) {
-  return {
-    items: state.pizzas.items
-  }
-};
-
-export default connect(MSTP, {setPizzas})(App);
-
-
-// function MDTP(dispatch) {
-//   return {
-//     setPizzas: (items) => dispatch(setPizzasAC(items))
-//   }
-// };
