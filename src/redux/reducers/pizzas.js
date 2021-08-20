67178b4976b6455c2e007a27462436879d84ed2e
resufer <66846932+resufer@users.noreply.github.com>
2021-08-20 13:35:41 +0300

    Create pizzas.js

---
+++ src/redux/reducers/pizzas.js
@@ -0,0 +1,19 @@
+let initialState = {
+  items: [],
+  isLoaded: false
+}
+
+export default function pizzas(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_PIZZAS':
+      return {
+        ...state,
+        items: action.payload,
+        isLoaded: true
+      }
+    default:
+      return {
+        ...state
+      }
+  }
+};
