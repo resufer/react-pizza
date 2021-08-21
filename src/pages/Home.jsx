b277f3cd9ff2cfdae61eadd55278e3c327e47eca
resufer <66846932+resufer@users.noreply.github.com>
2021-08-21 17:45:09 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Categories, SortPopup, PizzaBlock } from '../Components';
+import { setCategory } from '../redux/actions/filters';
+
+const categoryNames = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
+const sortItems = [
+  { name: 'популярности', type: 'popular' },
+  { name: 'цене', type: 'price' },
+  { name: 'алфавиту', type: 'alphabet' }
+];
 
 export default function Home() {
   let dispatch = useDispatch();
   let pizzas = useSelector(state => state.pizzas.items);
 
+  let onSelectCategory = React.useCallback(ind => dispatch(setCategory(ind)), []);
+
   return (
     <div className="container">
       <div className="content__top">
         <Categories
-          items={[
-            'Все',
-            'Мясные',
-            'Вегетарианские',
-            'Гриль',
-            'Острые',
-            'Закрытые',
-          ]}
+          onSelectCategory={onSelectCategory}
+          items={categoryNames}
         />
-        <SortPopup items={[
-          { name: 'популярности', type: 'popular' },
-          { name: 'цене', type: 'price' },
-          { name: 'алфавиту', type: 'alphabet'}]} />
+        <SortPopup items={sortItems} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
